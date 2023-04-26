dadadc6cf31237a384d70ed659668823a74db280
alisonjordan <alisonjor@gmail.com>
2023-04-25 22:19:22 -0300

    atualização do front: botões de rádio e div de perguntas

--- includes/formulario.php
+++ includes/formulario.php
@@ -12,283 +12,364 @@
   <h2 class="mt-3"><?=TITLE?></h2>
 
   <form method="post">
-
+  <div class="row">
     <!--PERGUNTA 1-->
-    <div id="div_pergunta1" class="form-group">
+    <div class="col-sm">
+    </div>
+
+
+    <div id="div_pergunta1" class="form-group col-3">
       <label>Como você avalia a qualidade do atendimento que recebeu?</label>
 
-      
-            <label class="form-control">
-              <input type="radio" id="pergunta1" name="resposta1" value="Excelente" > Excelente
+      <p>
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" id="pergunta1" name="resposta1" value="Excelente"> Excelente
             </label>
           
-
+            <p>
 
           
-            <label class="form-control">
-              <input type="radio" id="pergunta1" name="resposta1" value="Bom" > Bom
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" id="pergunta1" name="resposta1" value="Bom" > Bom
             </label>
         
 
+            <p>
           
-          
-            <label class="form-control">
-              <input type="radio" id="pergunta1" name="resposta1" value="Regular" > Regular
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" id="pergunta1" name="resposta1" value="Regular" > Regular
             </label>
           
 
+          <p>
           
-          
-            <label class="form-control">
-              <input type="radio" id="pergunta1" name="resposta1" value="Ruim" > Ruim
-            </label>
-          
-
-          
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" id="pergunta1" name="resposta1" value="Ruim" > Ruim
+            </label>        
     </div>
 
+    <div class="col-sm">
+    </div>
+    
+  
+  </div>
+
+  <div class="row">
     <!--PERGUNTA 2-->
-    <div id="div_pergunta2" style="display: none;" class="form-group">
+
+    <div class="col-sm">
+    </div>
+
+
+    <div id="div_pergunta2" style="display: none;" class="form-group col-3">
       <label>O atendente foi prestativo e solícito em ajudar com suas questões e problemas?</label>
 
+      <p>
       
-      
-            <label class="form-control">
-              <input type="radio" id="pergunta2" name="resposta2" value="Excelente" > Excelente
+      <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" id="pergunta2" name="resposta2" value="Excelente" > Excelente
             </label>
          
+            <p>
 
-
-            <label class="form-control">
-              <input type="radio" id="pergunta2" name="resposta2" value="Bom" > Bom
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" id="pergunta2" name="resposta2" value="Bom" > Bom
             </label>
           
-
+            <p>
           
           
-            <label class="form-control">
-              <input type="radio" id="pergunta2" name="resposta2" value="Regular" > Regular
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" id="pergunta2" name="resposta2" value="Regular" > Regular
             </label>
           
-
+            <p>
           
           
-            <label class="form-control">
-              <input type="radio" id="pergunta2" name="resposta2" value="Ruim" > Ruim
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" id="pergunta2" name="resposta2" value="Ruim" > Ruim
             </label>
           
      
     </div>
 
+    <div class="col-sm">
+    </div>
+
+    </div>
+
+    <div class="row">
     <!--PERGUNTA 3-->
-    <div id="div_pergunta3" style="display: none;" class="form-group">
+    <div class="col-sm">
+    </div>
+
+    <div id="div_pergunta3" style="display: none;" class="form-group col-3">
       <label>O atendente conseguiu responder suas perguntas e resolver seus problemas de forma satisfatória?</label>
 
-      
-            <label class="form-control">
-              <input type="radio" name="resposta3" value="Excelente" > Excelente
+      <p>
+      <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta3" value="Excelente" > Excelente
             </label>
          
-
+            <p>
 
          
-            <label class="form-control">
-              <input type="radio" name="resposta3" value="Bom" > Bom
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta3" value="Bom" > Bom
             </label>
           
 
+            <p>
           
-          
-            <label class="form-control">
-              <input type="radio" name="resposta3" value="Regular" > Regular
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta3" value="Regular" > Regular
             </label>
           
-
+            <p>
           
           
-            <label class="form-control">
-              <input type="radio" name="resposta3" value="Ruim" > Ruim
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta3" value="Ruim" > Ruim
             </label>
           
       
 
     </div>
 
+    <div class="col-sm">
+    </div>
+    
+  
+  </div>
+
+
+    <div class="row">
     <!--PERGUNTA 4-->
-    <div id="div_pergunta4" style="display: none;" class="form-group">
+    <div class="col-sm">
+    </div>
+
+
+    <div id="div_pergunta4" style="display: none;" class="form-group col-3">
       <label>O tempo de espera para ser atendido foi adequado?</label>
 
-      
-            <label class="form-control">
-              <input type="radio" name="resposta4" value="Excelente" > Excelente
+      <p>
+      <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta4" value="Excelente" > Excelente
             </label>
           
 
-
+            <p>
         
-            <label class="form-control">
-              <input type="radio" name="resposta4" value="Bom" > Bom
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta4" value="Bom" > Bom
             </label>
           
-
+            <p>
           
           
-            <label class="form-control">
-              <input type="radio" name="resposta4" value="Regular" > Regular
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta4" value="Regular" > Regular
             </label>
          
 
+            <p>
           
-          
-            <label class="form-control">
-              <input type="radio" name="resposta4" value="Ruim" > Ruim
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta4" value="Ruim" > Ruim
             </label>
           
       
 
     </div>
 
+    <div class="col-sm">
+    </div>
+    
+  
+  </div>
 
+
+    <div class="row">
     <!--PERGUNTA 5-->
-    <div id="div_pergunta5" style="display: none;" class="form-group">
+    <div class="col-sm">
+    </div>
+
+
+    <div id="div_pergunta5" style="display: none;" class="form-group col-3">
       <label>O atendimento foi realizado de forma cordial e respeitosa?</label>
 
-      
-            <label class="form-control">
-              <input type="radio" name="resposta5" value="Excelente" > Excelente
+      <p>
+      <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta5" value="Excelente" > Excelente
             </label>
           
 
-
+            <p>
           
-            <label class="form-control">
-              <input type="radio" name="resposta5" value="Bom" > Bom
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta5" value="Bom" > Bom
             </label>
         
-
+            <p>
           
       
-            <label class="form-control">
-              <input type="radio" name="resposta5" value="Regular" > Regular
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta5" value="Regular" > Regular
             </label>
           
 
-          
+            <p>
          
-            <label class="form-control">
-              <input type="radio" name="resposta5" value="Ruim" > Ruim
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta5" value="Ruim" > Ruim
             </label>
         
       
 
     </div>
 
+    <div class="col-sm">
+    </div>
+    
+  
+  </div>
 
 
+    <div class="row">
     <!--PERGUNTA 6-->
-    <div id="div_pergunta6" style="display: none;" class="form-group">
-      <label>O atendente ofereceu alternativas ou soluções criativas para o seu problema?</label>
+    <div class="col-sm">
+    </div>
 
+
+    <div id="div_pergunta6" style="display: none;" class="form-group col-3">
+      <label>O atendente ofereceu alternativas ou soluções criativas para o seu problema?</label>
+      <p>
       
-            <label class="form-control">
-              <input type="radio" name="resposta6" value="Excelente" > Excelente
+      <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta6" value="Excelente" > Excelente
             </label>
           
-
+            <p>
 
        
-            <label class="form-control">
-              <input type="radio" name="resposta6" value="Bom" > Bom
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta6" value="Bom" > Bom
             </label>
           
 
-          
+            <p>
          
-            <label class="form-control">
-              <input type="radio" name="resposta6" value="Regular" > Regular
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta6" value="Regular" > Regular
             </label>
       
-
+            <p>
           
          
-            <label class="form-control">
-              <input type="radio" name="resposta6" value="Ruim" > Ruim
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta6" value="Ruim" > Ruim
             </label>
          
       
 
     </div>
 
+    <div class="col-sm">
+    </div>
+    
+  
+  </div>
 
 
+    <div class="row">
     <!--PERGUNTA 7-->
-    <div id="div_pergunta7" style="display: none;" class="form-group">
+    <div class="col-sm">
+    </div>
+
+
+    <div id="div_pergunta7" style="display: none;" class="form-group col-3">
       <label>O atendimento atendeu às suas expectativas?</label>
 
-      
-            <label class="form-control">
-              <input type="radio" name="resposta7" value="Excelente" > Excelente
+      <p>
+      <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta7" value="Excelente" > Excelente
             </label>
          
 
-
+            <p>
           
-            <label class="form-control">
-              <input type="radio" name="resposta7" value="Bom" > Bom
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta7" value="Bom" > Bom
+            </label>
+          
+            <p>
+          
+          
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta7" value="Regular" > Regular
             </label>
           
 
+            <p>
           
-          
-            <label class="form-control">
-              <input type="radio" name="resposta7" value="Regular" > Regular
-            </label>
-          
-
-          
-          
-            <label class="form-control">
-              <input type="radio" name="resposta7" value="Ruim" > Ruim
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta7" value="Ruim" > Ruim
             </label>
          
       
 
     </div>
 
+    <div class="col-sm">
+    </div>
+    
+  
+  </div>
 
+    <div class="row">
     <!--PERGUNTA 8-->
-    <div id="div_pergunta8" style="display: none;" class="form-group">
-      <label>Quais as chances de você indicar o centro de formação SENAI, para um amigo ou familiar?</label>
+    <div class="col-sm">
+    </div>
 
+
+    <div id="div_pergunta8" style="display: none;" class="form-group col-3">
+      <label>Quais as chances de você indicar o centro de formação SENAI, para um amigo ou familiar?</label>
+      <p>
       
-            <label class="form-control">
-              <input type="radio" name="resposta8" value="Excelente" > Excelente
+      <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta8" value="Excelente" > Excelente
             </label>
           
-
+            <p>
 
           
-            <label class="form-control">
-              <input type="radio" name="resposta8" value="Bom" > Bom
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta8" value="Bom" > Bom
             </label>
        
 
+            <p>
           
-          
-            <label class="form-control">
-              <input type="radio" name="resposta8" value="Regular" > Regular
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta8" value="Regular" > Regular
             </label>
           
 
+            <p>
           
-          
-            <label class="form-control">
-              <input type="radio" name="resposta8" value="Ruim" > Ruim
+            <label class="form-control btn btn-outline-primary">
+              <input type="radio" class="btn-check" name="resposta8" value="Ruim" > Ruim
             </label>
          
       
 
     </div>
+
+    <div class="col-sm">
+    </div>
+    
+  
+  </div>
 
     
     <div id="div_submit" style="display: none;" class="form-group">
