3547c649c661605670723cc47ffc8d5c6548ac44
alisonjordan <alisonjor@gmail.com>
2023-05-10 17:28:34 -0300

    remoção de código para testes

--- includes/relatorio.php
+++ includes/relatorio.php
@@ -1,23 +1,4 @@
-<?php
 
-echo "Excelente: " . $respostas1['Excelente'];
-echo "Bom: " . $respostas1['Bom'];
-echo "Regular: " . $respostas1['Regular'];
-echo "Ruim: " . $respostas1['Ruim'];
-
-
-?>
-
-
-<?php
-
-echo "Excelente: " . $respostas2['Excelente'];
-echo "Bom: " . $respostas2['Bom'];
-echo "Regular: " . $respostas2['Regular'];
-echo "Ruim: " . $respostas2['Ruim'];
-
-
-?>
 <main>
 
   
